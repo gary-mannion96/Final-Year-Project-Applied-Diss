56f80965a389599fb05cb989f7f2bbab12db398e
Gary Mannion <gkmannion@hotmail.com>
2019-01-09 13:25:08 +0000

    feed calculator

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html'
 })
 export class FeedPage {
-  selectedItem: any;
-  icons: string[];
+  cattle: number;
+  silage: number;
+  feedValue: number;
+  feedMessage: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor() {
+  }
 
+  calculateFeed(){
+    if (this.silage > 0 && this.cattle > 0) {
+      let finalFeed = this.silage / (this.cattle / 100 * this.cattle / 100);
+      this.feedValue = parseFloat(finalFeed.toFixed(2));
+      this.setFeedMessage();
+    }
+  }
+
+  private setFeedMessage() {
+    if (this.feedValue < 18.5) {
+      this.feedMessage = "Underweight"
+    }
+  
+    if (this.feedValue > 18.5 && this.feedValue < 25) {
+      this.feedMessage = "Normal"
+    }
+  
+    if (this.feedValue > 25 && this.feedValue < 30) {
+      this.feedMessage = "Overweight"
+    }
+  
+    if (this.feedValue > 30) {
+      this.feedMessage = "Obese"
+    }
   }
 }
