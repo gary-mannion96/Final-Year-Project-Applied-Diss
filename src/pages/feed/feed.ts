86bd3c486c08d7f20c20485a7be2df7513934abb
Gary Mannion <gkmannion@hotmail.com>
2019-01-14 16:01:39 +0000

    bales amount

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -8,7 +8,7 @@ export class FeedPage {
   cattle: number;
   silage: number;
   feedValue: number;
-  feedMessage: string;
+  baleValue: number;
 
   constructor() {
   }
@@ -17,6 +17,7 @@ export class FeedPage {
     if (this.silage > 0 && this.cattle > 0) {
       let finalFeed = this.cattle * this.silage * 1.6;
       this.feedValue = parseFloat(finalFeed.toFixed(2));
+      this.baleValue = this.cattle * this.silage * 1.6 / 0.9;
     }
   }
 
