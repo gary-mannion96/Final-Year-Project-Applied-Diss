02edaa40bbf672d2f10ef7ed9777616e7780a371
Gary Mannion <gkmannion@hotmail.com>
2019-01-29 11:39:07 +0000

    medicine

--- src/pages/medicine/medicine.ts
+++ src/pages/medicine/medicine.ts
@@ -9,7 +9,6 @@ export class medicinePage {
   selectedItem: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
  
   }
  
