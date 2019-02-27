7c737f2ce26863550a765212323d166520520949
Derrick Conway <derrick.conway15@gmail.com>
2019-02-27 13:16:48 +0000

    provider

--- FarmWithEase/src/pages/tagging/tagging.ts
+++ FarmWithEase/src/pages/tagging/tagging.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddTagsPage } from '../add-tags/add-tags';
+import { TaggingProvider } from '../../providers/tagging/tagging';
 
 @Component({
   selector: 'page-tagging',
