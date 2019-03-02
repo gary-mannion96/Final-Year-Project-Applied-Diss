9552dd0392011bcd121461af11bcd5605af0bd5f
Derrick Conway <derrick.conway15@gmail.com>
2019-03-02 23:17:17 +0000

    tag ai page

---
+++ FarmWithEase/src/pages/add-ais/add-ais.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the AddTagsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-add-ais',
+  templateUrl: 'add-ais.html',
+})
+export class AddAisPage {
+
+  aiTagNumber: any;
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  }
+
+  save(): void {
+
+    let review = {
+      aiTagNumber: this.aiTagNumber,
+
+    };
+
+    this.viewCtrl.dismiss(review);
+
+  }
+
+  close(): void {
+    this.viewCtrl.dismiss();
+  }
+
+}
