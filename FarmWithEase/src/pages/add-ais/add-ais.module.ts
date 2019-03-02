b916fd1e9b3f54b87241d955172d24ab8095a52a
Derrick Conway <derrick.conway15@gmail.com>
2019-03-02 22:48:33 +0000

    tag ai page

---
+++ FarmWithEase/src/pages/add-ais/add-ais.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddAisPage } from './add-ais';
+
+@NgModule({
+  declarations: [
+    AddAisPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddAisPage),
+  ],
+})
+export class AddAisPageModule {}
