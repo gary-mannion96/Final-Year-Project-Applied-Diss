97a1917a949d5612c5f7fcd8d2d8086d1f73ed13
Derrick Conway <derrick.conway15@gmail.com>
2019-03-22 22:25:30 +0000

    report.ts

---
+++ FarmWithEase/src/pages/add-reports/add-reports.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the AddTagsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-add-reports',
+  templateUrl: 'add-reports.html',
+})
+export class AddReportsPage {
+
+  reportInfo: any;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  }
+
+  close(): void {
+    this.viewCtrl.dismiss();
+  }
+
+}
