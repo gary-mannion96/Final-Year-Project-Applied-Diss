c90ca480a39410e11bf1b481aed50772cc1f5f45
Gary Mannion <gkmannion@hotmail.com>
2019-03-26 09:29:27 +0000

    validation on registet page working correctly now

--- FarmWithEase/src/pages/register/register.ts
+++ FarmWithEase/src/pages/register/register.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController,LoadingController } from 'ionic-angular';
 import { User } from '../../Models/user';
 import {menuPage} from '../../pages/MainMenu/menu';
+import { Validators, FormBuilder, FormGroup} from '@angular/forms';
+import { matchingPasswords, emailValidator } from '../../validators/validators';
+
 
 @IonicPage()
 @Component({
@@ -12,20 +15,42 @@ import {menuPage} from '../../pages/MainMenu/menu';
 export class RegisterPage {
 
   user = {} as User;
+  validations_form: FormGroup;
 
-  constructor(private afAuth: AngularFireAuth,
-    public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth, 
+    public formBuilder: FormBuilder, private loadingController: LoadingController, private toast: ToastController) {
+      
+      this.validations_form = formBuilder.group({
+        email: ['', Validators.compose([Validators.required,  emailValidator])],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      }, {validator: matchingPasswords('password', 'confirmPassword')})
+  
+    }
 
-  async register(user: User){
-    try{
-    const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.user.email, this.user.password);
-    if(result){
-      this.navCtrl.setRoot(menuPage);
-    }
+async onSubmit(value: Object) {
+  let loading = this.loadingController.create({content : "Registering, please wait..."});
+  loading.present();    
+  try{
+    //Result object of creating a user with an email and password
+    const result = await this.afAuth.auth.createUserWithEmailAndPassword(value['email'], value['password']);
+    this.toast.create({
+      message: "Successfully Registered - Please Log In",
+      duration: 3000,
+      cssClass: "toast"   
+    }).present();
+    this.navCtrl.push(menuPage);
+    loading.dismissAll();
   }
-    catch (e){
-      console.error(e);
-    }
+  catch(e){
+    console.error("Error Registering: " + e);
+    loading.dismiss();
+    this.toast.create({
+      message: "Error Registering User - Please Try Again",
+      duration: 3000,
+      cssClass: "toast"   
+    }).present();
+    this.validations_form.reset();
   }
+}
 }
